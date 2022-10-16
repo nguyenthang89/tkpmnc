d2eb275a5f26e7fa53bcd8740493635c903a5bff
vudung061182 <vudung061182@gmail.com>
2022-10-16 09:49:43 +0700

    xu ly dang nhap va dang ky

---
+++ client/client/index.js
@@ -0,0 +1,131 @@
+const express = require("express");
+const ejs = require("ejs");
+
+const bodyParser = require("body-parser");
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json())
+app.get("/", function(req, res){
+    res.render("index");
+});
+
+const http = require('http');
+const { LocalStorage } = require("node-localstorage");
+localStorage = new LocalStorage('./scratch');
+
+app.get("/", function(req, res){
+    res.render("/index");
+})
+
+app.post("/", function(req, res){
+    
+})
+
+app.post("/signin", function(req, res){
+    let username = req.body.username;
+    let password = req.body.password;
+    let data = {
+        username: username,
+        password: password
+    }
+    let jsonData = JSON.stringify(data);
+    let options = {
+        hostname: "127.0.0.1",
+        port: 8080,
+        path: "/api/auth/signin",
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    let request = http.request(options, (response)=>{
+        // console.log('StatusCode: ', response.statusCode);
+        if(response.statusCode === 200){
+            response.on('data', data => {
+               let dataQueue = JSON.parse(data);
+               localStorage.setItem('x-token', dataQueue.token);
+            })
+            res.render("dashboard");
+        }
+        else{
+            res.redirect("/");
+        }
+    })
+    request.on('error', (err)=>{
+        console.log(err);
+    })
+    request.write(jsonData);
+    request.end();
+})
+
+app.post("/signup", function(req, res){
+    let username = req.body.createUsername;
+    let email = req.body.createEmail;
+    let password = req.body.createPassword;
+    let role = req.body.selector;
+    let data = {
+        username: username,
+        email: email,
+        password: password,
+        roles: [role]
+    }
+    let jsonData = JSON.stringify(data);
+    let options = {
+        hostname: "127.0.0.1",
+        port: 8080,
+        path: "/api/auth/signup",
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    let request = http.request(options, (response)=>{
+        if(response.statusCode === 200){
+            response.on("data", (data)=>{
+                console.log(JSON.parse(data));
+            })
+            res.render("dashboard");
+        }
+        else{
+            res.redirect("/");
+        }
+    })
+    request.on("error", (err)=>{
+        console.log(err);
+    })
+    request.write(jsonData);
+    request.end();
+})
+
+app.post("/dashboard", function(req,res){
+    res.render("dashboard");
+})
+
+app.post("/form", function(req, res){
+    let customerName = req.body.customerName;
+    let customerPhone = req.body.customerPhone;
+    let addressStart = req.body.addressStart;
+    let addressEnd = req.body.addressEnd;
+    let typeCar = req.body.typeCar;
+    const data = {
+
+    }
+    res.render("form");
+})
+
+app.post("/customer", function(req, res){
+    res.render("customer");
+})
+
+app.post("/driver", function(req, res){
+    let driverFullName = req.body.driverFullName;
+    let driverPhone = req.body.driverPhone;
+    let driverAddress = req.body.driverAddress;
+
+    res.render("driver");
+})
+app.listen(3000, function(){
+    console.log("Server is running on port 3000");
+})
