6d39d9e08dc7a4af4beb93b32707bde3f490b81a
vudung061182 <vudung061182@gmail.com>
2022-11-20 15:12:40 +0700

    update code

--- client/controllers/adminController.js
+++ client/controllers/adminController.js
@@ -33,35 +33,62 @@ const dashboard = (req, res, next)=>{
         res.render("admin/dashboard-admin.hbs");
     }
 }
-const coordinator = (req, res, next)=>{
-    let url = "http://localhost:8080/api/admin/get-driver";
+const getTop5Driver = async (req, res, next)=>{
+    let url = "http://localhost:8080/api/admin/get-new-order";
     let options = {
         method: "POST",
         headers: {
-            "Content-Type": 'application/json',
-            'x-access-token': localStorage.getItem('x-token')
+            "Content-Type": "application/json",
+            "x-access-token": localStorage.getItem("x-token")
         }
+    }
+    let data = await fetch(url, options).then(response => response.json());
+    return data.responseData;
+    
+}
+const coordinator = async (req, res, next)=>{
+    let dataCustomer = await getTop5Driver();
+    let data = {
+        lastName: dataCustomer.lastName,
+        firstName: dataCustomer.firstName,
+        from: dataCustomer.departure,
+        to: dataCustomer.destination,
+        phone: dataCustomer.phone,
+        lat: dataCustomer.startLat,
+        long: dataCustomer.startLong,
+        loai_xe: dataCustomer.loai_xe
+    }
+    let url = "http://localhost:8080/api/driver/top-nearby";
+    let options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "x-access-token": localStorage.getItem("x-token")
+        },
+        body: JSON.stringify(data)
     }
     fetch(url, options)
     .then(response => response.json())
-    .then(data => res.render('admin/coordinator.hbs', {data: data}))
-    .catch(err => {
-        console.log(err);
-    })
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
 }
 const bookCar = (req, res, next)=>{
     res.render("admin/book-car.hbs");
 }
-
-const postBookCar = (req, res, next)=>{
+const getLatLong = async (req, res, next)=>{
+    let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${req}&key=AIzaSyBG0r53JaAzCbXYHkvzSXkumHmfgHFe-lE`;
+    let responseData = await fetch(url).then(response => response.json());
+    return responseData.results[0].geometry.location;
+}
+const postBookCar = async (req, res, next)=>{
     let lastName = req.body.lastName;
     let firstName = req.body.firstName;
     let from = req.body.from;
     let to = req.body.to;
     let phone = req.body.phone;
     let loai_xe = req.body.loaixe;
-    let lat = 11.89462;
-    let long = 106.755173;
+    let latLngForm = await getLatLong(from);
+    let latLngTo = await getLatLong(to);
     localStorage.setItem('phone-customer', phone);
     let jsonDataBookCar = JSON.stringify({
         lastName: lastName,
@@ -69,8 +96,10 @@ const postBookCar = (req, res, next)=>{
         from: from,
         to: to,
         phone: phone,
-        lat: lat,
-        long: long,
+        startLat: latLngForm.lat,
+        startLong: latLngForm.lng,
+        endLat: latLngTo.lat,
+        endLong: latLngTo.lng,
         loai_xe: loai_xe
     });
     
@@ -93,34 +122,9 @@ const postBookCar = (req, res, next)=>{
     })
 }
 
-// const chooseDriver = (req, res, next) =>{
-//     let driverID = req.body.driverID;
-//     let data = {
-//         driverID: driverID
-//     }
-//     let url = "http://localhost:8080/api/admin/send-message";
-//     let options = {
-//         method: "POST",
-//         headers: {
-//             'Content-Type': 'application/json',
-//             'x-access-token': localStorage.getItem('x-token')
-//         },
-//         body: JSON.stringify(data)
-//     }
-//     fetch(url, options)
-//     .then(response => response.json())
-//     .then(data => {
-//         res.render("admin/chooseDriver", {
-//             message: data.data.responseData.message
-//         })
-//     })
-//     .catch(err => console.log(err));
-// }
-
 module.exports = {
     dashboard,
     coordinator,
     bookCar,
-    postBookCar,
-    // chooseDriver
+    postBookCar
 }
--- client/controllers/driverController.js
+++ client/controllers/driverController.js
@@ -31,41 +31,56 @@ const updateInfo = (req, res, next)=>{
     res.render("driver/update-info.hbs");
 }
 
-const postUpdateInfo = (req, res, next)=>{
-    let driverId = localStorage.getItem('id');
-    let lastName = req.body.lastName;
-    let firstName = req.body.firstName;
-    let birthDate = req.body.birthDate;
-    let address = req.body.address;
-    let phone = req.body.phone;
-    let photo = "";
-    let dataJson = JSON.stringify({
-        driverId: driverId,
-        lastName: lastName,
-        firstName: firstName,
-        birthDate: birthDate,
-        address: address,
-        phone: phone,
-        photo: photo
-    });
-    let url = "http://localhost:8080/api/driver/info-driver-upd";
-    let options = {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json',
-            'x-access-token': localStorage.getItem('x-token')
-        },
-        body: dataJson
+const getLatLong = async (req, res, next)=>{
+    let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${req}&key=AIzaSyBG0r53JaAzCbXYHkvzSXkumHmfgHFe-lE`;
+    let responseData = await fetch(url).then(response => response.json());
+    return responseData.results[0].geometry.location;
+}
+
+const postUpdateInfo = async(req, res, next)=>{
+    try{
+        let driverId = localStorage.getItem('id');
+        let lastName = req.body.lastName;
+        let firstName = req.body.firstName;
+        let birthDate = req.body.birthDate;
+        let address = req.body.address;
+        let phone = req.body.phone;
+        let photo = "";
+        let latLng = await getLatLong(address);
+        let dataJson = JSON.stringify({
+            driverId: driverId,
+            lastName: lastName,
+            firstName: firstName,
+            birthDate: birthDate,
+            address: address,
+            phone: phone,
+            photo: photo,
+            lat: latLng.lat,
+            long: latLng.lng
+        });
+        let url = "http://localhost:8080/api/driver/info-driver-upd";
+        let options = {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': localStorage.getItem('x-token')
+            },
+            body: dataJson
+        }
+        fetch(url, options)
+        .then(response => response.json())
+        .then(data => {
+            localStorage.setItem('info-driver', dataJson);
+            res.render('driver/update-info.hbs', {message: data.message});
+        })
+        .catch(err => {
+            console.log(err);
+        })
     }
-    fetch(url, options)
-    .then(response => response.json())
-    .then(data => {
-        localStorage.setItem('info-driver', dataJson);
-        res.render('driver/update-info.hbs', {message: data.message});
-    })
-    .catch(err => {
+    catch(err){
         console.log(err);
-    })
+    }
+    
 }
 
 
